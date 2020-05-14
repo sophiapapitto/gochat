5c0c97e34bf9551fe36c19decbfb4dcff337a3e8
sophia papitto <sophiapapitto@berkeley.edu>
2020-05-14 15:41:08 -0700

    parse command line arg
    Checks whether client or server is being invoked.
    Prints error if number of arguments != 1.

--- gochat.go
+++ gochat.go
@@ -1,12 +1,40 @@
 package main
 
-import "fmt"import (
+import (
     "fmt"
-    "math"
+    "os"
 )
 
+func server() {
+
+}
 
 func main() {
+    fmt.Println("at beginning of main")
 
-    fmt.Println(add(42, 13))
+    args := os.Args[1:]
+    //TODO throw errors differently? return
+    if len(args) != 1 {
+        fmt.Printf("ERROR: %d arguments given; should be %d\n", len(args), 1)
+        fmt.Println("returning from main prematurely")
+        return
+
+    }
+
+
+    actor := args[0]
+
+    if actor == "server" {         
+        fmt.Println("is server")
+    } else if actor == "client" {
+        fmt.Println("is client")
+
+    } else {
+        fmt.Println("ERROR: arg 0 malformed; must be 'client' or 'server")
+        fmt.Println("returning from main prematurely")
+        return
+    }
+
+    fmt.Println("returning from main successfully")
+
 }
